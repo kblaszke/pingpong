d34ae168bc84139cda2e906593f8a7423513aea4
kblaszke <kblaszke@plusnet.pl>
2017-03-31 14:09:52 +0200

    [master] Project creation

---
+++ src/main/java/pl/blaszak/spring/PingpongApplication.java
@@ -0,0 +1,12 @@
+package pl.blaszak.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingpongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingpongApplication.class, args);
+	}
+}
